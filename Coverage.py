a25bcdbb9241be5756838fe94bf5f1018151cbfc
muregad <47953868+muregad@users.noreply.github.com>
2021-05-24 17:09:38 +0200

    Add files via upload

---
+++ Coverage.py
@@ -0,0 +1,65 @@
+class Coverage:
+    def __init__(self):
+
+        # I ===> 0
+        # S ===> 1
+        # E ===> 2
+        # M ===> 3
+
+        self.uncovered = []
+        self.uncovered2 = []
+        self.uncovered.append([0, 0, 0, 2])
+        self.uncovered.append([0, 0, 2, 0])
+        self.uncovered.append([0, 2, 0, 0])
+        self.uncovered.append([2, 0, 0, 0])
+        self.uncovered.append([0, 0, 0, 3])
+        self.uncovered.append([0, 0, 3, 0])
+        self.uncovered.append([0, 3, 0, 0])
+        self.uncovered.append([3, 0, 0, 0])
+
+        for i in [0, 1]:
+            for j in [0, 1]:
+                for k in [0, 1]:
+                    for m in [0, 1]:
+                        if (i + j + k + m) != 1:
+                            self.uncovered.append([i, j, k, m])
+
+        for state in self.uncovered:
+            for proc in range(4):
+                ss = state.copy()
+                ss.append(proc)
+                for j in range(2):
+                    sss = ss.copy()
+                    sss.append(j)
+                    self.uncovered2.append(sss)
+
+        self.uncoveredMapping = {}
+        self.uncoveredMapped = []
+        cnt = 0
+        for state in self.uncovered2:
+            self.uncoveredMapping[self.lst_to_string(state)] = cnt
+            self.uncoveredMapped.append(cnt)
+            cnt += 1
+
+        self.total = len(self.uncoveredMapped)
+
+
+    def lst_to_string(self, lst):
+        ss = ""
+        for elmnt in lst:
+            ss += str(int(elmnt))
+        return ss
+
+    def sample(self, state):
+        encodedState = self.uncoveredMapping[self.lst_to_string(state)]
+        if encodedState in self.uncoveredMapped:
+            self.uncoveredMapped.remove(encodedState)
+        return
+
+    def get_coverage(self):
+        return (1.0 - len(self.uncoveredMapped) / self.total) * 100.0
+
+# tst = Coverage()
+#
+# print(tst.uncovered)
+# print(tst.uncovered2[80])
--- Main.py
+++ Main.py
@@ -1,10 +1,46 @@
 from CPU import CPU
 from random import randint
+import numpy as np
+from Coverage import Coverage
+
+# I ===> 0
+# S ===> 1
+# E ===> 2
+# M ===> 3
 
 
-cpu = CPU(num_of_cores=4)
+encode = {"I": 0, "S": 1, "E": 2, "M": 3}
 
-n = 20
-for i in range(n):
-    cpu.bus.instruction(processor=randint(0, 3), r_w=randint(0, 1), address=8, value=randint(0, 15))
+
+num_of_cores = 4
+
+cpu = CPU(num_of_cores=num_of_cores)
+t = 10000
+
+x = np.zeros((t, 6))
+y = np.zeros((t, 4))
+cvg = Coverage()
+for i in range(t):
+    x[i, 0:4] = [encode[cpu.processors[0].Cache.getState(address=8)], encode[cpu.processors[1].Cache.getState(address=8)], encode[cpu.processors[2].Cache.getState(address=8)], encode[cpu.processors[3].Cache.getState(address=8)]]
+    processor = np.random.randint(0, 4)
+    r_w = np.random.randint(0, 2)
+    x[i, 4] = processor
+    x[i, 5] = r_w
+    # if(r_w == 2):
+    #     cpu.processors[0].Cache.reset()
+    #     cpu.processors[1].Cache.reset()
+    #     cpu.processors[2].Cache.reset()
+    #     cpu.processors[3].Cache.reset()
+    # else:
+    print(x[i,0:4])
+    cvg.sample(x[i])
+    print(f"Iteration {i} ===> Coverage: {cvg.get_coverage()}")
+    cpu.bus.instruction(processor=processor, r_w=r_w, address=8, value=randint(0, 15))
     cpu.printStatus()
+    y[i] = [encode[cpu.processors[0].Cache.getState(address=8)], encode[cpu.processors[1].Cache.getState(address=8)], encode[cpu.processors[2].Cache.getState(address=8)], encode[cpu.processors[3].Cache.getState(address=8)]]
+    print(y[i])
+    print("------------------------------")
+
+# print(cvg.uncoveredMapped)
+# print(x)
+# print(y)
